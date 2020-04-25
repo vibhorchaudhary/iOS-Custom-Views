af998db557850ffd1b9d513714f5af1f606a8c3f
Vibhor Chaudhary <10243674+vibhorchaudhary@users.noreply.github.com>
2020-04-25 23:35:51 +0530

    Update CustomDesignButton.swift

--- CustomDesignButton.swift
+++ CustomDesignButton.swift
@@ -1,6 +1,5 @@
 //
 //  CustomDesignButton.swift
-//  Splash
 //
 //  Created by Vibhor Chaudhary on 20/04/20.
 //  Copyright © 2020 Vibhor Chaudhary. All rights reserved.
@@ -142,14 +141,5 @@ import UIKit
         }
         
     }
-    
-    
-    /*
-     // Only override draw() if you perform custom drawing.
-     // An empty implementation adversely affects performance during animation.
-     override func draw(_ rect: CGRect) {
-     // Drawing code
-     }
-     */
     
 }
